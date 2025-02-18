371c0c4856f809f09a036e1ed67fadf87f027482
UIUX Lab <uiuxlab@gmail.com>
2025-02-18 11:34:00 +0800

    Update custom.webpack.config.js
    Do not package third-party frameworks into trunks

--- custom.webpack.config.js
+++ custom.webpack.config.js
@@ -28,10 +28,18 @@ if (fs.existsSync(fileConfig)) {
                                 .replace('static/js/[name].[contenthash:8].js', 'static/js/[name].js')
                                 .replace('static/css/[name].[contenthash:8].css', 'static/css/[name].css')
                                 .replace(/externals\:.*?entry\:\s*paths.appIndexJs,/, 'entry: paths.appIndexJs,')
+
+                                // Do not package third-party frameworks into trunks
+                                .replace(/minimize\:\s*[\s\S]*?minimizer\:/, `minimize: isEnvProduction, splitChunks:{chunks:"all",cacheGroups:{default:false,vendors:false,defaultVendors:{name:'main',chunks:'all',test:/.*/,priority:10,reuseExistingChunk:false}}},runtimeChunk:false, minimizer:`)
+
+                                // alias
                                 .replace(/alias:\s*{[\s\S]*?'react-native': 'react-native-web',/, `alias: {
                                     '@': path.resolve(__dirname, paths.appSrc),
                                     'react-native': 'react-native-web',`)
                                 .replace(/entry\:\s*paths.appIndexJs,/, CodeAsString);
+
+                                    
+
     
                 fs.writeFile(fileConfig, result, function (err) {
                     if (err) return console.log(err)
